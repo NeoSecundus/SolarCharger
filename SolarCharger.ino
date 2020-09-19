36afb8b738b4e692d8680b055737b229c80eb34a
Nikolas Teuschl <niklas.teuschl@hotmail.com>
2020-09-19 18:58:00 +0200

    Added Wemos D1 Mini implementation of Display and ACS712 module.

--- SolarCharger.ino
+++ SolarCharger.ino
@@ -1,14 +1,65 @@
-#include <ACS712.h>
-#include <SPI.h>
+#include "src/ACSReader.cpp"
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 
-void setup() {
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 32 
+#define OLED_RESET 4 
 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+ACSReader acsr(5.06, A0, 3.2, 1023, 100);
+int infoDelay = 0;
+
+
+void setup() {
+    Serial.begin(9600);
+
+    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+        Serial.println("SSD1306 allocation failed");
+    }
+
+    display.display();
+    delay(2000);
+    display.clearDisplay();
+    display.display();
+
+    acsr.acs.setMidPoint(840);
 }
 
 void loop() {
+    float mA = acsr.getMedianDC(10, 100);
 
+    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+        Serial.println("SSD1306 allocation failed");
+    } else {
+        char toPrint[21];
+        snprintf(toPrint, 21, "A: %dmA\nW: %.02fW", (int)mA, acsr.getWatt());
+        printDisplay(toPrint);
+        delay(1000);
+
+        if (infoDelay > 4) {
+            snprintf(toPrint, 21, "Runtime:\n%s", acsr.getWorkingTime());
+            printDisplay(toPrint);
+            delay(3000);
+
+            snprintf(toPrint, 21, "Supplied:\n%s", acsr.getSuppliedWattString());
+            printDisplay(toPrint);
+            delay(3000);
+
+            infoDelay = 0;
+        } else {
+            infoDelay++;
+        }
+    }
+}
+
+void printDisplay(const char* str) {
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    display.print(str);
+    display.display();
 }
---
+++ src/ACSReader.cpp
@@ -0,0 +1,64 @@
+#include <ACS712.h>
+
+class ACSReader {
+    private:
+    float voltage;
+    float lastVal = 0;
+    float suppliedWatts = 0;
+    int lastProcessTime = 0;
+
+    public:
+    ACS712 acs;
+    
+    ACSReader(float refVoltage, uint8_t pin, float voltage, uint16_t maxADC, uint8_t mVperA): acs(pin, voltage, maxADC, mVperA) {
+        this->voltage = refVoltage;
+    }
+
+    float getMedianDC(int loops = 4, int msInterval = 50) {
+        float mA = 0;
+        int curProcessTime = 0;
+        for (int i = 0; i < loops; i++) {
+            mA += (float)acs.mA_DC();
+            delay(msInterval);
+        }
+        lastVal = mA/loops;
+
+        curProcessTime = millis();
+        suppliedWatts += getWatt() * (curProcessTime - lastProcessTime) / 1000;
+        lastProcessTime = curProcessTime;
+
+        return lastVal;
+    }
+
+    float getWatt() {
+        return lastVal * voltage / 1000;
+    }
+
+    float getSuppliedWatt() {
+        return suppliedWatts;
+    }
+
+    char* getSuppliedWattString() {
+        char res[10];
+        if (abs(suppliedWatts) < 1000) {
+            sprintf(res, "%.2fW", suppliedWatts);
+        } else if (abs(suppliedWatts) < 1000000) {
+            sprintf(res, "%.2fkW", suppliedWatts/1000);
+        } else {
+            sprintf(res, "%.2fMW", suppliedWatts/1000000);
+        }
+
+        return res;
+    }
+
+    char* getWorkingTime() {
+        char res[9];
+        int curProcessSecs = (int)lastProcessTime / 1000;
+        int hours = floor(curProcessSecs / 3600);
+        int minutes = floor((curProcessSecs % 3600) / 60);
+        int seconds = curProcessSecs % 60;
+
+        sprintf(res, "%02d:%02d:%02d", hours, minutes, seconds);
+        return res;
+    }
+};
